f3a2e2773af82821b8d94af66f6960c4d16489a0
danfham <dan.cunningham91@icloud.com>
2023-09-16 12:04:51 -0400

    defined hotel schema in data model

--- models/Hotels.js
+++ models/Hotels.js
@@ -1,26 +1,25 @@
-/* Schema structure
------Hotels
-name:{
-    string
-    required
-}
-duration of stay{
-    start date
-    end date
-}
-location{
-    number
-    lat
-    lon
-}
-Price{
-    number
-}
+const mongoose = require('mongoose')
 
+const hotelSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required
+    },
+    stayDuration: {
+        type: Date,
+        required
+    },
+    lon: {
+        type: Number,
+        required
+    },
+    lat: {
+        type: Number,
+        required
+    },
+    price: {
+        type: Number
+    }
+})
 
-
-
-
-
-
-*/
+module.exports = mongoose.model('Hotels',hotelSchema)
