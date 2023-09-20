6a698a27841ad418e605e311472862a2801ded9c
zwithe <zwithe133@gmail.com>
2023-09-20 17:40:12 -0400

    moved contents of index to trip summary

--- views/tripSummary.jsx
+++ views/tripSummary.jsx
@@ -4,9 +4,33 @@ const Default = require('./layouts/default')
 function tripSummary (){
     return(
         <Default>
-            <h1>
-                Trip Summary Placehoder
-            </h1>
+            <header>
+                <h1>Your Trip Planning App</h1>
+            </header>
+            <div className="main-container">
+                <div className="trip-details">
+                <div className="card">
+                    <h2>Hotel Name</h2>
+                    <p>Location: City Center</p>
+                    <p>Check-In Date: 2023-10-15</p>
+                    <p>Check-Out Date: 2023-10-18</p>
+                </div>
+                <div className="card">
+                    <h2>Activity Name</h2>
+                    <p>Description: Explore the city's landmarks.</p>
+                </div>
+                </div>
+                <div className="form-container">
+                <h2>Add New Data</h2>
+                <form>
+                    <input type="text" placeholder="Hotel Name"/>
+                    <input type="text" placeholder="Location"/>
+                    <input type="date" placeholder="Check-In Date"/>
+                    <input type="date" placeholder="Check-Out Date"/>
+                    <input type="submit" className="button" value="Add Hotel"/>
+                </form>
+                </div>
+            </div>
         </Default>
     )
 }
