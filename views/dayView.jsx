07ad35eae771ee6acf1954def028a98414b94167
danfham <dan.cunningham91@icloud.com>
2023-09-25 10:20:15 -0400

    dayView page renders all day information that has been generated

--- views/dayView.jsx
+++ views/dayView.jsx
@@ -1,23 +1,27 @@
 const React = require('react')
 const Default = require('./layouts/default')
+const Itinerary = require('../models/Itinerary')
 
-function dayView ({day}){
-    /*const dayDisplay = day.map(day =>{
+function dayView ({Days, Activities}){
+    console.log(Days)
+    console.log(Activities)
+    const daysDisplay = Days.map(Days =>{
         return(
-             <li key={day._id} className="form-container" >
+             <li key={Days._id} className="form-container" >
                  <a>
-                     <h2>{day._id}</h2>
+                    <h2>{JSON.stringify(Days._id)}</h2>
+                    <h2>{JSON.stringify(Days.date)}</h2>
                  </a>
              </li>
          )
-    })*/
+    })
     return(
         <Default>
             <header>
-                <h1>Your Daily Planner</h1>
+                <h1>Your Trip Planning App</h1>
             </header>
             <div className="main-container">
-                {day.name}
+                {daysDisplay}
             </div>
         </Default>
     )
